9964ed614ee934179fc807d8b8c63c5e693a080f
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-03 03:19:42 -0500

    add: database event logging

--- api/src/services/db.ts
+++ api/src/services/db.ts
@@ -3,6 +3,33 @@ import * as dotenv from "dotenv";
 
 dotenv.config()
 
+/**
+ * Connection events
+ */
+// upon successful connection
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose successfully connected to database ' + process.env.DB_NAME);
+});
+
+// upon error in connection
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+// upon connection disconnect
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// upon node process termination
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+}); 
+
+
 export const connect = async () => {
     return mongoose.connect(process.env.MONGODB_URI as string)
 }
