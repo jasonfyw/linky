f494416b2f1c69cc98837f4ac4cd0d17d14afac5
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-04 04:43:06 -0500

    add: endpoint to open a shortened link

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,9 +1,24 @@
 import { Request, Response, Router } from 'express';
+import { ShortLink } from '../models/ShortLink';
 
 const router = Router()
 
-router.get('/', async (req: Request, res: Response) => {
-    return res.status(200).send('hello world')
+/**
+ * Redirect if alias exists in database
+ */
+router.get('/:id', async (req: Request, res: Response) => {
+    const id = req?.params?.id
+
+    try {
+        const url = await ShortLink.findOne({ alias: id })
+        if (url) {
+            return res.redirect(url.url)
+        } else {
+            res.status(404).send('This link does not exist')
+        }
+    } catch (error) {
+        res.status(404).send('This link does not exist')
+    }
 })
 
 export { router }
