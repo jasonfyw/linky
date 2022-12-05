4fab46806f36c107345c071988d799d3adac6e4a
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-05 05:12:49 -0500

    add: post endpoint to create new shortened url

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Request, Response, Router } from 'express';
+import { customAlphabet } from 'nanoid';
 import { ShortLink } from '../models/ShortLink';
 
 const router = Router()
+
+const generator = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10)
 
 /**
  * Redirect if alias exists in database
@@ -18,6 +21,29 @@ router.get('/:id', async (req: Request, res: Response) => {
         }
     } catch (error) {
         res.status(404).send('This link does not exist')
+    }
+})
+
+/**
+ * Add a new alias url pair to that database
+ */
+router.post('/new/', async (req: Request, res: Response) => {
+    let { url } = req.body
+    const isValidUrl = (s: string) => {
+        try { return Boolean(new URL(s)) } catch (e) { return false }
+    }
+
+    try {
+        if (isValidUrl(url)) {
+            const alias = generator()
+            const newShortLink = { alias, url }
+            const shortLink = await ShortLink.create(newShortLink)
+            res.status(200).send(shortLink)
+        } else {
+            res.status(401).send('Invalid URL')
+        }
+    } catch (error) {
+        res.status(500).send(error)
     }
 })
 
