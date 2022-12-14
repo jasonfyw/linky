5072131237159e4ede9a158a4068cfae8f3e7cd2
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-13 22:44:41 -0500

    docs: add comment to helper function

--- app/src/helpers/links.helpers.ts
+++ app/src/helpers/links.helpers.ts
@@ -1,3 +1,8 @@
+/**
+ * Formats URLs by appending http:// if http(s) prefix is not present
+ * @param url string
+ * @returns <url> with http:// prefix
+ */
 export const includeHTTP = (url: string): string => {
     if (!url.match(/^(http|https):\/\//)) {
         console.log('http://'.concat(url))
