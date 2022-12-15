564af240a665f44e87747c8f5aa4f4b737c03d02
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-14 19:30:12 -0500

    add: copy to clipboard button to LinkCard

--- app/src/components/LinkDisplay/LinkCard.tsx
+++ app/src/components/LinkDisplay/LinkCard.tsx
@@ -4,9 +4,18 @@ import {
     CardHeader,
     Heading,
     Link,
-    Text
+    Text,
+    Button,
+    Flex,
+    Spacer,
+    Center,
+    useToast
 } from '@chakra-ui/react';
+import {
+    CopyIcon
+} from '@chakra-ui/icons';
 import React from 'react';
+import { useCopyToClipboard } from 'usehooks-ts';
 
 interface LinkCardProps {
     shortLink: string,
@@ -14,16 +23,58 @@ interface LinkCardProps {
 }
 
 const LinkCard = (props: LinkCardProps) => {
+    const [, copy] = useCopyToClipboard()
+    const toast = useToast()
+    
+    const copyLink = () => {
+        try {
+            // copy shortened link to clipboard
+            copy(props.shortLink)
+            // display a success toast with the copied link
+            toast({
+                title: 'Link copied to clipboard!',
+                status: 'success',
+                duration: 2000,
+                isClosable: true,
+            })
+        } catch (e) {
+            // display an error toast
+            toast({
+                title: 'An error occurred. Please try again',
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            })
+        }
+    }
+
     return (
         <Card>
-            <CardHeader pb={0} pt={4}>
-                <Heading size={'md'}><Link href={props.shortLink}>{ props.shortLink }</Link></Heading>
+            <CardHeader pb={0} pt={2}>
+                <Flex>
+                    <Center>
+                        <Heading size={'md'}><Link href={props.shortLink}>{props.shortLink}</Link></Heading>
+                    </Center>
+                    <Spacer/>
+                    <Button
+                        colorScheme='cyan'
+                        size={'sm'}
+                        leftIcon={<CopyIcon/>}
+                        onClick={copyLink}
+                    >
+                        Copy
+                    </Button>
+                </Flex>
             </CardHeader>
-            <CardBody pt={2} pb={3}>
-                <Text><Link href={props.link}>{ props.link }</Link></Text>
+            <CardBody pt={2} pb={4}>
+                <Text><Link href={props.link}>{props.link}</Link></Text>
             </CardBody>
         </Card>
     );
 }
 
 export default LinkCard;
+
+function toast(arg0: { title: string; description: JSX.Element; status: string; duration: number; isClosable: boolean; }) {
+    throw new Error('Function not implemented.');
+}
