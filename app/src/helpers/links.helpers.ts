bd3ee2d4c957aeacb44dafa25dc9fb637d876ff9
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-15 20:40:03 -0500

    refactor: remove console output

--- app/src/helpers/links.helpers.ts
+++ app/src/helpers/links.helpers.ts
@@ -5,7 +5,6 @@
  */
 export const includeHTTP = (url: string): string => {
     if (!url.match(/^(http|https):\/\//)) {
-        console.log('http://'.concat(url))
         return 'http://'.concat(url)
     }
     return url
