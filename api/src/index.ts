69ced1b638366dac76a93944b8f26fd7b78ff230
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-12-28 01:37:55 +0100

    fix: get favicons working

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from "path";
+import favicon from "serve-favicon";
+
 import { connect } from "./services/db";
 import { router } from "./routes";
 import { corsOptions } from "./config/corsOptions";
-import path from "path";
+
 
 dotenv.config()
 
@@ -24,6 +27,7 @@ const app = express();
  */
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(favicon(path.resolve(__dirname, '../frontend_build/favicon.ico')))
 app.use(router)
 app.use(express.static(path.resolve(__dirname, '../frontend_build')))
 
